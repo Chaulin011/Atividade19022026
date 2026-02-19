e2b529092f1b34e8c20bde95c1bccda0e4ec1c5e
Chaulin011 <147333601+Chaulin011@users.noreply.github.com>
2026-02-19 20:50:37 -0300

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Neymar na Copa do Mundo</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="container">
+
+<?php
+$nome = "Neymar Jr";
+?>
+
+<h1><?php echo $nome; ?> na Copa do Mundo</h1>
+
+<img src="ney.jpg" alt="">
+
+<p>
+<?php echo $nome; ?> é um jogador brasileiro muito conhecido.
+Ele representou o Brasil em Copas do Mundo.
+</p>
+
+<p>
+Na Copa de 2014, realizada no Brasil, ele foi um dos principais jogadores da seleção.
+</p>
+
+<p>
+Também participou das Copas de 2018 e 2022.
+</p>
+
+<a href="https://www.fifa.com" target="_blank">
+    Site Oficial da FIFA
+</a>
+
+</div>
+
+</body>
+</html>
